c78c48562cdf702ce363184f94156a1958903df6
Axell Martinez <axell.martinezavila@gmail.com>
2022-02-12 02:03:14 -0600

    created hidden files

--- compile.py
+++ compile.py
@@ -1,7 +1,7 @@
 import os
 
-os.system("g++ -o compile encryption.cpp")
-os.system("g++ -o compile2 unencrypt.cpp")
-os.system("g++ -o maincompile interface.cpp")
+os.system("g++ -o compile .encryption.cpp")
+os.system("g++ -o compile2 .unencrypt.cpp")
+os.system("g++ -o maincompile .interface.cpp")
 os.system("./maincompile")
 
